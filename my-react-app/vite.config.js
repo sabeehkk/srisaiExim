afb8f93a4984b79c5465c5a0bd8e7d4cfe4260d6
Sabeeh KK <sabeehkadungalloor573@gmail.com>
2025-10-30 13:20:00 +0530

    updation for vite config

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -8,3 +8,12 @@ export default defineConfig({
     outDir: 'dist'
   }
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
